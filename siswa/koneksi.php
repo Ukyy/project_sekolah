38674f47eef30e520a68f7d1aa5cb3d12747ac96
Lucky <luckyputraryansyah123@gmail.com>
2023-01-30 09:57:19 +0700

    menambahkan PHP PDO

--- siswa/koneksi.php
+++ siswa/koneksi.php
@@ -1,2 +1,22 @@
 <?php
-$koneksi = mysqli_connect("localhost","root","","sekolah_ukyy");
+//$koneksi = mysqli_connect("localhost", "root", "", "sekolah_ukyy");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=sekolah_ukyy", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+$sql = "SELECT * FROM siswa";
+$sql = $conn->prepare($sql);
+$sql->execute();
+while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+    print_r($row);
+}
+die('tes');
+?>
